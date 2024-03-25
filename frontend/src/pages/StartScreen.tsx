42a75682574f8dab1570b9bd85b505886f1dba84
ingobecker <mail@ingobecker.de>
2024-03-25 15:48:48 +0100

    add react-oauth/google@latest add log in with google button connect to oauth with google

--- frontend/src/pages/StartScreen.tsx
+++ frontend/src/pages/StartScreen.tsx
@@ -1,17 +1,26 @@
-import {Avatar, Button} from "@mui/material";
+import {Avatar} from "@mui/material";
+import {GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
 import "./StartScreen.css"
 
 export default function StartScreen() {
+
     return (
-        <>
+        <GoogleOAuthProvider clientId={"912133900656-llv5gkrj9mp9qbke1lmkr989dbhp3fhf.apps.googleusercontent.com"}>
             <div className={"startpage"}>
                 <div className={"startpage-wrapper"}>
                     <Avatar className={"startpage-wrapper-logo"}/>
                     <h1 className={"startpage-wrapper-text"}>Track My Meal</h1>
-                    <Button variant="contained">Login</Button>
+                    <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            console.log(credentialResponse);
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                    />
                 </div>
             </div>
-        </>
+        </GoogleOAuthProvider>
 
     );
 }
