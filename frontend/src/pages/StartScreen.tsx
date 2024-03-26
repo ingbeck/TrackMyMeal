95d6e34ca3df28edcf35fabd33ff4647d859fe15
ingobecker <mail@ingobecker.de>
2024-03-26 16:25:09 +0100

    delete Google button

--- frontend/src/pages/StartScreen.tsx
+++ frontend/src/pages/StartScreen.tsx
@@ -1,26 +1,22 @@
-import {Avatar} from "@mui/material";
-import {GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
+import {Avatar, Button} from "@mui/material";
 import "./StartScreen.css"
 
-export default function StartScreen() {
+type StartScreenProps = {
+    login: () => void,
+}
+
+export default function StartScreen(props: Readonly<StartScreenProps>) {
 
     return (
-        <GoogleOAuthProvider clientId={"912133900656-llv5gkrj9mp9qbke1lmkr989dbhp3fhf.apps.googleusercontent.com"}>
+        <>
             <div className={"startpage"}>
                 <div className={"startpage-wrapper"}>
                     <Avatar className={"startpage-wrapper-logo"}/>
                     <h1 className={"startpage-wrapper-text"}>Track My Meal</h1>
-                    <GoogleLogin
-                        onSuccess={credentialResponse => {
-                            console.log(credentialResponse);
-                        }}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                    />
+                    <Button variant={"contained"} onClick={props.login}>Login</Button>
                 </div>
             </div>
-        </GoogleOAuthProvider>
+        </>
 
     );
 }
