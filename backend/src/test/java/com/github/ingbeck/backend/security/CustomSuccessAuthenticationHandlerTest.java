2fbe0d1451aaf84ee78dcc046078880bbadae445
ingobecker <mail@ingobecker.de>
2024-03-26 17:27:08 +0100

    add update unittests

--- backend/src/test/java/com/github/ingbeck/backend/security/CustomSuccessAuthenticationHandlerTest.java
+++ backend/src/test/java/com/github/ingbeck/backend/security/CustomSuccessAuthenticationHandlerTest.java
@@ -39,50 +39,7 @@ class CustomSuccessAuthenticationHandlerTest {
         try (AutoCloseable ignored = MockitoAnnotations.openMocks(this)) {
 
             CustomSuccessAuthenticationHandler customAuthenticationHandler = new CustomSuccessAuthenticationHandler(userRepository);
-            ReflectionTestUtils.setField(customAuthenticationHandler, "appURL", "http://localhost:5173");
 
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("sub", "987654321");
-            attributes.put("name", "Max Mustermann");
-            attributes.put("birthdate", "1991-14-07");
-            attributes.put("picture", "https://example.com/mustermax.jpg");
-            attributes.put("gender", null);
-            attributes.put("height", 0);
-            attributes.put("weight", 0);
-            attributes.put("activityLevel", null);
-            attributes.put("isNewUser", true);
-            when(authentication.getPrincipal()).thenReturn(oAuth2User);
-            when(oAuth2User.getAttributes()).thenReturn(attributes);
-
-            AppUser user = new AppUser(
-                    "987654321",
-                    "Max Mustermann",
-                    "1991-14-07",
-                    "https://example.com/mustermax.jpg",
-                    null,
-                    0,
-                    0,
-                    null,
-                    true
-            );
-
-            when(userRepository.findById("987654321")).thenReturn(Optional.of(user));
-
-            customAuthenticationHandler.onAuthenticationSuccess(request, response, authentication);
-
-            verify(response).sendRedirect("http://localhost:5173/registration/987654321");
-            ArgumentCaptor<AppUser> userCaptor = ArgumentCaptor.forClass(AppUser.class);
-            verify(userRepository).save(userCaptor.capture());
-            AppUser savedUser = userCaptor.getValue();
-            assertFalse(savedUser.isNewUser());
-        }
-    }
-
-    @Test
-    void testOnAuthenticationSuccessWhenUserIsNotNew() throws Exception {
-        try (AutoCloseable ignored = MockitoAnnotations.openMocks(this)) {
-
-            CustomSuccessAuthenticationHandler customAuthenticationHandler = new CustomSuccessAuthenticationHandler(userRepository);
 
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("sub", "987654321");
@@ -114,6 +71,50 @@ class CustomSuccessAuthenticationHandlerTest {
             customAuthenticationHandler.onAuthenticationSuccess(request, response, authentication);
 
             verify(response).sendRedirect(anyString());
+            ArgumentCaptor<AppUser> userCaptor = ArgumentCaptor.forClass(AppUser.class);
+            verify(userRepository).save(userCaptor.capture());
+            AppUser savedUser = userCaptor.getValue();
+            assertFalse(savedUser.isNewUser());
+        }
+    }
+
+    @Test
+    void testOnAuthenticationSuccessWhenUserIsNotNew() throws Exception {
+        try (AutoCloseable ignored = MockitoAnnotations.openMocks(this)) {
+
+            CustomSuccessAuthenticationHandler customAuthenticationHandler = new CustomSuccessAuthenticationHandler(userRepository);
+            ReflectionTestUtils.setField(customAuthenticationHandler, "appURL", "http://localhost:5173");
+
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("sub", "987654321");
+            attributes.put("name", "Max Mustermann");
+            attributes.put("birthdate", "1991-14-07");
+            attributes.put("picture", "https://example.com/mustermax.jpg");
+            attributes.put("gender", null);
+            attributes.put("height", 0);
+            attributes.put("weight", 0);
+            attributes.put("activityLevel", null);
+            attributes.put("isNewUser", false);
+            when(authentication.getPrincipal()).thenReturn(oAuth2User);
+            when(oAuth2User.getAttributes()).thenReturn(attributes);
+
+            AppUser user = new AppUser(
+                    "987654321",
+                    "Max Mustermann",
+                    "1991-14-07",
+                    "https://example.com/mustermax.jpg",
+                    null,
+                    0,
+                    0,
+                    null,
+                    true
+            );
+
+            when(userRepository.findById("987654321")).thenReturn(Optional.of(user));
+
+            customAuthenticationHandler.onAuthenticationSuccess(request, response, authentication);
+
+            verify(response).sendRedirect("http://localhost:5173/registration/987654321");
             ArgumentCaptor<AppUser> userCaptor = ArgumentCaptor.forClass(AppUser.class);
             verify(userRepository).save(userCaptor.capture());
             AppUser savedUser = userCaptor.getValue();
