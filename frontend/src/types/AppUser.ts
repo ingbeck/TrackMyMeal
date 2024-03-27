ae6ee5809164c2096113886722a1d1cf2f10a6b8
ingobecker <mail@ingobecker.de>
2024-03-27 15:19:54 +0100

    add refactor gender type

---
+++ frontend/src/types/AppUser.ts
@@ -0,0 +1,13 @@
+export type AppUser = {
+    id : string,
+    name : string,
+    age : number,
+    avatarUrl : string,
+    gender : string,
+    height : number,
+    weight : number,
+    activityLevel : string,
+    bmr : number,
+    bmrWithActivity : number,
+    isNewUser : boolean
+}
