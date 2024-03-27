007341b30e21b3d1acbc79380877d04886e0b243
ingobecker <mail@ingobecker.de>
2024-03-27 10:43:20 +0100

    add integration test

--- backend/src/main/java/com/github/ingbeck/backend/controller/UserController.java
+++ backend/src/main/java/com/github/ingbeck/backend/controller/UserController.java
@@ -2,7 +2,6 @@ package com.github.ingbeck.backend.controller;
 
 import com.github.ingbeck.backend.model.AppUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
