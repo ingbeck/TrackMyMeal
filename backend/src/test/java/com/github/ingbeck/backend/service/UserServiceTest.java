9ce8b7191a5f68e7bda0a55739231702e46f8c4a
ingobecker <mail@ingobecker.de>
2024-03-27 13:10:21 +0100

    add unit test

---
+++ backend/src/test/java/com/github/ingbeck/backend/service/UserServiceTest.java
@@ -0,0 +1,77 @@
+package com.github.ingbeck.backend.service;
+
+import com.github.ingbeck.backend.model.ActivityLevel;
+import com.github.ingbeck.backend.model.AppUser;
+import com.github.ingbeck.backend.model.AppUserCreateDto;
+import com.github.ingbeck.backend.repository.UserRepository;
+import com.nimbusds.openid.connect.sdk.claims.Gender;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+    private final UserRepository repo = mock(UserRepository.class);
+    private final UserService service = new UserService(repo);
+
+    @Test
+    void createUser_whenUserExistsAndCalledWithValidAppUserCreateDto_thenReturnAppUserWithDtosValue(){
+        //GIVEN
+        AppUserCreateDto appUserCreateDto = new AppUserCreateDto(
+                32,
+                Gender.MALE,
+                180,
+                90,
+                ActivityLevel.ATHLETE
+        );
+
+        AppUser expected = getAppUser(appUserCreateDto);
+        when(repo.findById("1")).thenReturn(Optional.of(expected));
+        when(repo.save(expected)).thenReturn(expected);
+
+        //WHEN
+        AppUser actual = service.createUser("1", appUserCreateDto);
+
+        //THEN
+        verify(repo).findById("1");
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void createUser_whenCalledWithInvalidIID_thenThrowException() throws NoSuchElementException{
+        //GIVEN
+        AppUserCreateDto appUserCreateDto = new AppUserCreateDto(
+                32,
+                Gender.MALE,
+                180,
+                90,
+                ActivityLevel.ATHLETE
+        );
+
+        assertThrows(NoSuchElementException.class, () -> {service.createUser("2", appUserCreateDto);});
+    }
+
+    private static AppUser getAppUser(AppUserCreateDto appUserCreateDto) {
+        int bmr = (int)(66.47 + 13.75 * appUserCreateDto.weight() + 5.003 * appUserCreateDto.height() - 6.755 * appUserCreateDto.age());
+        int bmrWithActivity = (int)(bmr * appUserCreateDto.activityLevel().getLevel());
+
+        AppUser expected = new AppUser(
+                "1",
+                "Max Mustermann",
+                32,
+                "https://example.com/mustermax.jpg",
+                Gender.MALE,
+                180,
+                90,
+                ActivityLevel.ATHLETE,
+                bmr,
+                bmrWithActivity,
+                false
+        );
+        return expected;
+    }
+
+}
