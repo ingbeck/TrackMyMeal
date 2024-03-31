81d34be556fb06ec8895e27f167e25d57a7d2445
ingobecker <mail@ingobecker.de>
2024-03-31 17:24:38 +0200

    fix reliability rating issue

--- frontend/src/pages/StartScreen.tsx
+++ frontend/src/pages/StartScreen.tsx
@@ -12,7 +12,7 @@ export default function StartScreen(props: Readonly<StartScreenProps>) {
                 <div className={"startpage-wrapper"}>
                     <img src={"../src/assets/Logo.svg"} className={"startpage-wrapper-logo"}/>
                     <h1 className={"startpage-wrapper-text"}>Track My Meal</h1>
-                    <div className={"btn-login"} onClick={props.login}>
+                    <div className={"btn-login"} onClick={props.login} onKeyDown={props.login}>
                         <img src={"../src/assets/google.svg"}/>
                         <span>Mit Google anmelden</span>
                     </div>
