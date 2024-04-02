2905ab6456efc73be1ae7eb3910228555225e974
ingobecker <mail@ingobecker.de>
2024-04-02 17:44:18 +0200

    hide date mask in desktop improve validation

--- frontend/src/components/FloatingDatePicker.tsx
+++ frontend/src/components/FloatingDatePicker.tsx
@@ -10,7 +10,9 @@ type FloatingDatePickerProps = {
 export default function FloatingDatePicker(props: Readonly<FloatingDatePickerProps>) {
 
     const [value, setValue] = useState('');
+    const [isClicked, setIsClicked] = useState<boolean>(false)
     const hasError = props.error !== "" && props.error !== undefined
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
 
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         const value = e.target.value
@@ -18,13 +20,39 @@ export default function FloatingDatePicker(props: Readonly<FloatingDatePickerPro
         props.handleChange(e);
     }
 
+    function getDateToday():string{
+        const today = new Date()
+        const day = today.getDay()
+        const month = today.getMonth() + 1
+        const yyyy = today.getFullYear()
+
+        let dd:string = day.toString();
+        let mm:string = month.toString();
+
+        if (day < 10) {
+            dd = '0' + day;
+        }
+
+        if (month < 10) {
+            mm = '0' + month;
+        }
+
+        return yyyy+"-"+mm+"-"+dd;
+    }
+
     return (
        <div className={hasError ? "input-container-error" : "input-container"}>
            <input className={"input-container-datepicker"}
-                  type={"date"}
+                  type={isClicked || isIOS? "date" : "text"}
                   value={value}
-                  id="input"
+                  id="dateInput"
                   name={props.name}
+                  max={getDateToday()}
+                  onClick={() => setIsClicked(true)}
+                  onBlur={() => {
+                      if(value === ''){
+                          setIsClicked(false)
+                  }}}
                   onChange={handleChange}/>
            <label className={value && 'filled'} htmlFor={"input"}>
                {hasError ? props.error : props.label}
