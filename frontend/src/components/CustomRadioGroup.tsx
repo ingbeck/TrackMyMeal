ed4256ef7f686dafa85e2fe7a8559af2407da1a6
ingobecker <mail@ingobecker.de>
2024-04-02 16:02:56 +0200

    add custom radio inout display validation message

---
+++ frontend/src/components/CustomRadioGroup.tsx
@@ -0,0 +1,40 @@
+import "./CustomRadioGroup.css"
+import {ChangeEvent} from "react";
+
+type CustomRadioGroupProps = {
+    label : string,
+    choices : { id: number, label: string, value: string }[],
+    handleChange: (event: ChangeEvent<HTMLInputElement>) => void,
+    error? : string
+}
+export default function CustomRadioGroup(props: Readonly<CustomRadioGroupProps>) {
+
+    const hasError = props.error !== "" && props.error !== undefined
+
+    return (
+        <>
+            <div className={hasError ? "container-error" : "container"}>
+                <div className={"radio-container"}>
+                    <label className={"radio-container-label"}>{props.label}</label>
+                    {
+                        props.choices.map((choice) =>
+                            <div className={"radio-container-choice"}>
+                                <input type={"radio"}
+                                       key={choice.id}
+                                       id={choice.label}
+                                       name={"activityLevel"}
+                                       value={choice.value}
+                                       onChange={props.handleChange}
+                                />
+                                <label htmlFor={choice.label}>{choice.label}</label>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+            {hasError && <label className={"radio-container-label-error"}>{props.error}</label>}
+        </>
+
+
+    );
+}
