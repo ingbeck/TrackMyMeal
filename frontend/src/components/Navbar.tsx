c127cff041d56b649f96f55799ec8f179c31ca11
ingobecker <mail@ingobecker.de>
2024-04-03 14:59:37 +0200

    fix sonarcloud complains

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,34 +15,34 @@ export default function Navbar(props: Readonly<NavbarProps>) {
     return (
         <nav className={"navbar"}>
             <div className={"navbar-wrapper"}>
-                <a onClick={() => navigate("/home")}
+                <button onClick={() => navigate("/home")}
                    className={props.currentRoute == "http://localhost:5173/home" ? "navbar-item-active" : "navbar-item"}>
                     <div>
                         <img src={TodayIcon} alt={""}/>
                         <label>Heute</label>
                     </div>
-                </a>
-                <a onClick={() => navigate("/calendar")}
+                </button>
+                <button onClick={() => navigate("/calendar")}
                    className={props.currentRoute == "http://localhost:5173/calendar" ? "navbar-item-active" : "navbar-item"}>
                     <div>
                         <img src={CalendarIcon} alt={""}/>
                         <label>Kalender</label>
                     </div>
-                </a>
-                <a onClick={() => navigate("/recipes")}
+                </button>
+                <button onClick={() => navigate("/recipes")}
                    className={props.currentRoute == "http://localhost:5173/recipes" ? "navbar-item-active" : "navbar-item"}>
                     <div>
                         <img src={RecipeIcon} alt={""}/>
                         <label>Rezepte</label>
                     </div>
-                </a>
-                <a onClick={() => navigate("/profile")}
+                </button>
+                <button onClick={() => navigate("/profile")}
                    className={props.currentRoute == "http://localhost:5173/profile" ? "navbar-item-active" : "navbar-item"}>
                     <div>
                         <img src={ProfileIcon} alt={""}/>
                         <label>Profil</label>
                     </div>
-                </a>
+                </button>
             </div>
 
         </nav>
