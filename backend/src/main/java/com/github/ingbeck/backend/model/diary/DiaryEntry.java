eb7e4315aa6f730244c15535834d5e32ac0ba126
ingobecker <mail@ingobecker.de>
2024-04-09 11:28:08 +0200

    refactor date

--- backend/src/main/java/com/github/ingbeck/backend/model/diary/DiaryEntry.java
+++ backend/src/main/java/com/github/ingbeck/backend/model/diary/DiaryEntry.java
@@ -1,10 +1,8 @@
 package com.github.ingbeck.backend.model.diary;
-
-import java.time.LocalDate;
 import java.util.List;
 
 public record DiaryEntry(
-        LocalDate date,
+        String date,
         List<FoodItem> foodItems,
         int totalCalories
 ) {
