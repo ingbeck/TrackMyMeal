1601ea4efab76af837c9f6a619ad69a4ca3b96b1
ingobecker <mail@ingobecker.de>
2024-04-11 14:31:29 +0200

    add findDiaryByUserId

--- backend/src/main/java/com/github/ingbeck/backend/service/DiaryService.java
+++ backend/src/main/java/com/github/ingbeck/backend/service/DiaryService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +14,46 @@ public class DiaryService {
 
     private final DiaryRepository diaryRepository;
 
+    public Diary getDiaryByUserId(String userId){
+        return diaryRepository.findDiaryByUserId(userId);
+    }
     public Diary createNewDiary(String userId){
         List<DiaryEntry> emptyDiaryEntries = new ArrayList<>();
 
         return diaryRepository.save(new Diary(null, userId, emptyDiaryEntries));
+    }
+
+    public DiaryEntry getDiaryEntryByDate(String userId, String date){
+        return diaryRepository.findDiaryByUserId(userId).diaryEntries()
+                .stream().filter(diaryEntry -> diaryEntry.date().equals(date))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Für diesen Tag gibt es keine Einträge!"));
+    }
+
+
+    public Diary updateDiaryEntry(String userId, String date, List<FoodItem> newFoodItems){
+        Diary diaryToUpdate = getDiaryByUserId(userId);
+        List<DiaryEntry> currentDiaryEntries = diaryToUpdate.diaryEntries();
+
+
+        List<DiaryEntry> newDiaryEntries;
+        int totalCalories;
+
+        try{
+            DiaryEntry diaryEntryToUpdate = getDiaryEntryByDate(userId,date);
+            List<FoodItem> updatedFoodItems = diaryEntryToUpdate.foodItems();
+            updatedFoodItems.addAll(newFoodItems);
+            totalCalories = updatedFoodItems.stream().map(FoodItem::calories).reduce(0, Integer::sum);
+            DiaryEntry updatedDiaryEntry = diaryEntryToUpdate.withFoodItems(updatedFoodItems).withTotalCalories(totalCalories);
+            newDiaryEntries = currentDiaryEntries.stream().map(entry -> entry.date().equals(date) ? updatedDiaryEntry : entry).toList();
+
+        }catch (NoSuchElementException e){
+            totalCalories = newFoodItems.stream().map(FoodItem::calories).reduce(0, Integer::sum);
+            newDiaryEntries = List.of(new DiaryEntry(date, newFoodItems, totalCalories));
+
+        }
+
+        return diaryRepository.save(getDiaryByUserId(userId).withDiaryEntries(newDiaryEntries));
     }
 
 }
