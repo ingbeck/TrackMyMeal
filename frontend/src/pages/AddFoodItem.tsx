1b971685c94e6a4efdfc1cc06308acb893030323
ingobecker <mail@ingobecker.de>
2024-04-12 10:23:07 +0200

    remove navbar on page AddFoodItem

--- frontend/src/pages/AddFoodItem.tsx
+++ frontend/src/pages/AddFoodItem.tsx
@@ -1,21 +1,44 @@
 import "./AddFoodItem.css"
 import {useNavigate} from "react-router-dom";
 import {AppUser} from "../types/AppUser.ts";
+import {useEffect} from "react";
 
 type AddFoodItemProps = {
+    setCurrentRoute : (url:string) => void,
     mealType : string,
     appUser : AppUser
 }
 function AddFoodItem(props: Readonly<AddFoodItemProps>) {
 
     const navigate = useNavigate()
+    const url = window.location.href;
+
+    useEffect(() => {
+        props.setCurrentRoute(url)
+    }, []);
+
+    function translateMealType(mealType : string) : string{
+        switch (mealType){
+            case "BREAKFAST":
+                return "Frühstück"
+            case "LUNCH":
+                return "Mittagessen"
+            case "DINNER":
+                return "Abendessen"
+            case "SNACK":
+                return "Snack"
+            default:
+                return ""
+        }
+
+    }
 
     return (
         <div className={"addfooditem"}>
             <div className={"addfooditem-header-wrapper"}>
                 <button onClick={() => navigate("/home/"+props.appUser.id)}>Zurück</button>
-                <h1>Essen hinzufügen</h1>
-                <label>{props.mealType}</label>
+                <h1>{translateMealType(props.mealType)}</h1>
+                <label>Warenkorb</label>
             </div>
         </div>
     );
