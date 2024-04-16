0297cd50328078f6b79d79e915ae587552a95036
ingobecker <mail@ingobecker.de>
2024-04-16 15:03:27 +0200

    redesign registration screen

--- frontend/src/components/registration/CustomRadioGroup.tsx
+++ frontend/src/components/registration/CustomRadioGroup.tsx
@@ -3,6 +3,7 @@ import {ChangeEvent} from "react";
 
 type CustomRadioGroupProps = {
     label : string,
+    name: string,
     choices : { id: number, label: string, value: string }[],
     handleChange: (event: ChangeEvent<HTMLInputElement>) => void,
     error? : string
@@ -13,15 +14,15 @@ export default function CustomRadioGroup(props: Readonly<CustomRadioGroupProps>)
 
     return (
         <>
-            <div className={hasError ? "container-error" : "container"}>
-                <div className={"radio-container"}>
-                    <label className={"radio-container-label"}>{props.label}</label>
+            <div className={"container"}>
+                <div className={hasError ? "radio-container-error" : "radio-container"}>
+                    <label id={"radio-label"}>{props.label}</label>
                     {
                         props.choices.map((choice) =>
                             <div className={"radio-container-choice"} key={choice.id}>
                                 <input type={"radio"}
                                        id={choice.label}
-                                       name={"activityLevel"}
+                                       name={props.name}
                                        value={choice.value}
                                        onChange={props.handleChange}
                                 />
@@ -31,7 +32,6 @@ export default function CustomRadioGroup(props: Readonly<CustomRadioGroupProps>)
                     }
                 </div>
             </div>
-            {hasError && <label className={"radio-container-label-error"}>{props.error}</label>}
         </>
 
 
