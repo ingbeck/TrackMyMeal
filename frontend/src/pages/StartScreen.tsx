ae69a6d518556dc4c13415457ee45c93feda7acf
ingobecker <mail@ingobecker.de>
2024-04-16 13:14:02 +0200

    redesign startpage

--- frontend/src/pages/StartScreen.tsx
+++ frontend/src/pages/StartScreen.tsx
@@ -1,5 +1,5 @@
 import "./StartScreen.css"
-import Logo from "../assets/Logo.svg"
+import Logo from "../assets/logo.png"
 import GoogleLogo from "../assets/google.svg"
 import {useEffect} from "react";
 
@@ -17,15 +17,25 @@ export default function StartScreen(props: Readonly<StartScreenProps>) {
     }, []);
 
     return (
-            <div className={"startpage"}>
-                <div className={"startpage-wrapper"}>
-                    <img src={Logo} className={"startpage-wrapper-logo"} alt={"Logo der App; eine Gabel, die von einem runden Fortschrittsbalken umgeben ist"}/>
-                    <h1 className={"startpage-wrapper-text"}>Track My Meal</h1>
-                    <button className={"btn-login"} onClick={props.login}>
-                        <img src={GoogleLogo} alt={"Logo von Google"}/>
-                        <span>Mit Google anmelden</span>
-                    </button>
+        <div className={"startpage"}>
+            <div className={"startpage-hero-wrapper"}>
+                <div className={"image-hero"}></div>
+                <div className={"logo-wrapper"}>
+                    <img src={Logo} className={"image-logo"}
+                         alt={"Logo der App"}/>
                 </div>
             </div>
+            <div className={"startpage-body-wrapper"}>
+                <div className={"headline-wrapper"}>
+                    <h1 className={"startpage-wrapper-text"}>Track My Meal</h1>
+                    <p>Dein mobiles Kalorientagebuch für eine&nbsp;ausgewogene Ernährung</p>
+                </div>
+                <button className={"btn-login"} onClick={props.login}>
+                    <img src={GoogleLogo} alt={"Logo von Google"}/>
+                    <span>Mit Google anmelden</span>
+                </button>
+                <span>© 2024 Ingo Becker</span>
+            </div>
+        </div>
     );
 }
