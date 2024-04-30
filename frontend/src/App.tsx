5d72e98776488b639a6ebe1f98211f03cb9ebdad
ingobecker <mail@ingobecker.de>
2024-04-30 11:43:11 +0200

    update task execution role arn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -56,7 +56,7 @@ export default function App() {
 
 
     function login(){
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+        const host = window.location.host === 'localhost:5173' ? 'https://localhost:8443' : window.location.origin
         window.open(host + "/oauth2/authorization/google", "_self")
     }
 
