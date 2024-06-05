736e67e734f256b99c48e4587b359e94c3d5de85
ingobecker <mail@ingobecker.de>
2024-06-05 15:58:02 +0200

    refactor button function

--- frontend/src/pages/CalendarScreen.tsx
+++ frontend/src/pages/CalendarScreen.tsx
@@ -29,28 +29,24 @@ export default function CalendarScreen(props: Readonly<CalendarScreenProps>) {
         props.getDiaryByUserId(params.id)
     }, [params.id]);
 
-    function buttonBackClick(){
+    function calenderButtonClick(type: "back" | "next"){
         let year = date.getFullYear();
         let month = date.getMonth();
 
-        if (month === 0) {
-            year--;
-            month = 11;
-        } else {
-            month--;
-        }
-        setDate(new Date(year, month));
-    }
-
-    function buttonNextClick(){
-        let year = date.getFullYear();
-        let month = date.getMonth();
-
-        if (month === 11) {
-            year++;
-            month = 0;
-        } else {
-            month++;
+        if(type === "next"){
+            if (month === 11) {
+                year++;
+                month = 0;
+            } else {
+                month++;
+            }
+        }else if(type === "back"){
+            if (month === 0) {
+                year--;
+                month = 11;
+            } else {
+                month--;
+            }
         }
 
         setDate(new Date(year, month));
@@ -61,16 +57,19 @@ export default function CalendarScreen(props: Readonly<CalendarScreenProps>) {
             <h1>Kalender</h1>
             <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
                 <h2>{formattedDateCaption(date)}</h2>
-                <div className={"modalAddFoodItem-btn-wrapper-horizontal"}>
-                    <button className={"cancel"} onClick={buttonBackClick}>{"<"}</button>
-                    <button className={"add"} onClick={() => setDate(new Date())}>Heute</button>
+                <div>
+                    <button onClick={() => calenderButtonClick("back")}>{"<"}</button>
+                    <button onClick={() => setDate(new Date())}>Heute</button>
                     {
-                        isCurrentMonth(date) && <button className={"cancel"} onClick={buttonNextClick}>{">"}</button>
+                        isCurrentMonth(date) ? <button onClick={() => calenderButtonClick("next")}>{">"}</button> : <button style={{visibility:"hidden"}}>{">"}</button>
                     }
                 </div>
             </div>
-            <CalendarView appUserCalories={props.appUser.bmrWithActivity} diaryEntries={props.diary.diaryEntries}
-                          date={date}/>
+            <CalendarView
+                appUserCalories={props.appUser.bmrWithActivity}
+                diaryEntries={props.diary.diaryEntries}
+                date={date}
+            />
         </div>
     );
 }
