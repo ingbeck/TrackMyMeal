cd9b93285fbd47afbce087ad0eefbd94a03a5645
ingobecker <mail@ingobecker.de>
2024-06-06 20:46:59 +0200

    render only buttons until today

--- frontend/src/components/CalendarDayCard.tsx
+++ frontend/src/components/CalendarDayCard.tsx
@@ -4,24 +4,36 @@ type CalendarDayCardProps = {
     calendarDay : CalendarDay,
     isToday : (day: number) => boolean,
     percentage: number,
-    clickCalendarDay: (day: number) => void
+    clickCalendarDay: (day: number) => void,
+    isLaterThanToday: (day: number) => boolean
 }
 
 export default function CalendarDayCard(props: Readonly<CalendarDayCardProps>) {
 
-    const progressFill: string = "linear-gradient(0deg, #4D2FFF3F "+props.percentage+"%, #FFF 0%)";
+    const progressFill: string = "linear-gradient(0deg, #4D2FFF3F "+props.percentage+"%, #eee 0%)";
+    const todayDate: string = "linear-gradient(0deg, #4A30FF 0%, #9018FE 31.5%, #C009FC 68.5%, #D161E2 100%)";
 
     return (
-        <button className={props.isToday(props.calendarDay.day) ? "day_selected" : "day"}
-                onClick={() => props.clickCalendarDay(props.calendarDay.day)}
-                style={!props.isToday(props.calendarDay.day)
+        <>
+            {
+                !props.isLaterThanToday(props.calendarDay.day)
                     ?
-                    {background: progressFill}
+                    <button className={"day"}
+                            onClick={() => props.clickCalendarDay(props.calendarDay.day)}
+                            style={!props.isToday(props.calendarDay.day)
+                                ?
+                                {background: progressFill}
+                                :
+                                {background: todayDate, color:"#fff"}}>
+                        <span>{props.calendarDay.day}</span>
+                    </button>
                     :
-                    {}}>
-            <span>{props.calendarDay.day}</span>
-        </button>
+                    <div className={"day"} style={{}}>
+                        <span>{props.calendarDay.day}</span>
+                    </div>
+            }
 
+        </>
     );
 }
 
