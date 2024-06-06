9994a1c3fe2fc205b7055d194d88d858feafd57e
ingobecker <mail@ingobecker.de>
2024-06-06 15:40:04 +0200

    change progress direction

--- frontend/src/components/CalendarDayCard.tsx
+++ frontend/src/components/CalendarDayCard.tsx
@@ -12,8 +12,14 @@ export default function CalendarDayCard(props: Readonly<CalendarDayCardProps>) {
     return (
         <button className={props.isToday(props.calendarDay.day) ? "day_selected" : "day"}
         onClick={() => props.clickCalendarDay(props.calendarDay.day)}>
-            <CircularProgress id={"backgroundCircle"} variant={"determinate"} value={100}/>
-            <CircularProgress id={"foregroundCircle"} variant={"determinate"} value={props.percentage} />
+            <CircularProgress id={"backgroundCircle"}
+                              variant={"determinate"}
+                              value={100}
+                              sx={{ scale: "-1 1" }}/>
+            <CircularProgress id={"foregroundCircle"}
+                              variant={"determinate"}
+                              value={props.percentage}
+                              sx={{ scale: "-1 1" }} />
             <span>{props.calendarDay.day}</span>
         </button>
     );
