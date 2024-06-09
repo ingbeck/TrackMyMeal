b7e301b1752e76953b92308e2b18b1e4c7dfbb71
ingobecker <mail@ingobecker.de>
2024-06-09 12:16:21 +0200

    add model meal

---
+++ backend/src/main/java/com/github/ingbeck/backend/model/meal/Meal.java
@@ -0,0 +1,15 @@
+package com.github.ingbeck.backend.model.meal;
+
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("meals")
+public record Meal(
+        String id,
+        String userId,
+        int totalCalories,
+        List<MealItem> mealItems
+) {
+}
