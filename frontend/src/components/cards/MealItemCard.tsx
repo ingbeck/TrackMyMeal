2c921c50e8f68111d1ba0c8ad7e906f92428871a
ingobecker <mail@ingobecker.de>
2024-06-13 15:34:38 +0200

    render food items to max given value

--- frontend/src/components/cards/MealItemCard.tsx
+++ frontend/src/components/cards/MealItemCard.tsx
@@ -9,29 +9,36 @@ type MealItemCardProps = {
 }
 export default function MealItemCard(props: Readonly<MealItemCardProps>) {
 
+    const numberMealItemsToRender: number = 4;
+
     useEffect(() => {
 
         highlightSearchText(props.searchText, props.meal.id);
 
     }, [props]);
 
-    function renderMealItems(){
-        let mealItemsToRender: MealItem[];
+    function renderMealItems(numberItemsToRender: number){
+        let mealItemsToRender : MealItem[];
+        let numberCommas : number;
 
-        if(props.meal.mealItems.length > 2){
-            mealItemsToRender = props.meal.mealItems.slice(0,2);
+        if(props.meal.mealItems.length > numberItemsToRender){
+            mealItemsToRender = props.meal.mealItems.slice(0,numberItemsToRender);
+            numberCommas = numberItemsToRender+1;
         }else{
             mealItemsToRender =props.meal.mealItems;
+            numberCommas = props.meal.mealItems.length;
         }
 
-        return mealItemsToRender.map(mealItem =>
-            <span key={mealItem.id} className={"meal_mealItem"}>{mealItem.name}</span>
+        return mealItemsToRender.map((mealItem, count) => {
+                count++;
+                return <span key={mealItem.id} className={"meal_mealItem"}>{mealItem.name}{count < numberCommas && ","}</span>
+            }
         )
     }
 
     return (
         <div className={"card meal"}>
-            <div className={"card-header mealCard_divider"}>
+        <div className={"card-header mealCard_divider"}>
                 <div className={"card-header-wrapper"}>
                     <label id={props.meal.id}>{props.meal.name}</label>
                     <span className={"gradient"}><span
@@ -39,8 +46,8 @@ export default function MealItemCard(props: Readonly<MealItemCardProps>) {
                 </div>
             </div>
             <div className={"meal_mealItem-wrapper"}>
-                {renderMealItems()}
-                {props.meal.mealItems.length > 2 && <span className={"meal_mealItem"}>...</span>}
+                {renderMealItems(numberMealItemsToRender)}
+                {props.meal.mealItems.length > numberMealItemsToRender && <span className={"meal_mealItem"}>...</span>}
             </div>
         </div>
     );
