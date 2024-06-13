5b36292cab7251dcbaeaf5e52f98a560d4461b44
ingobecker <mail@ingobecker.de>
2024-06-13 11:40:48 +0200

    add mealItems to card

--- frontend/src/components/cards/MealItemCard.tsx
+++ frontend/src/components/cards/MealItemCard.tsx
@@ -1,4 +1,4 @@
-import {Meal} from "../../types/Meal.ts";
+import {Meal, MealItem} from "../../types/Meal.ts";
 import "./MealItemCard.css"
 
 type MealItemCardProps = {
@@ -6,10 +6,33 @@ type MealItemCardProps = {
 }
 export default function MealItemCard(props: Readonly<MealItemCardProps>) {
 
+    function renderMealItems(){
+        let mealItemsToRender: MealItem[];
+
+        if(props.meal.mealItems.length > 2){
+            mealItemsToRender = props.meal.mealItems.slice(0,2);
+        }else{
+            mealItemsToRender =props.meal.mealItems;
+        }
+
+        return mealItemsToRender.map(mealItem =>
+            <span key={mealItem.id} className={"meal_mealItem"}>{mealItem.name}</span>
+        )
+    }
+
     return (
-        <div className={"mealItemCard"}>
-            <span>{props.meal.name}</span>
-            <span>{props.meal.totalCalories} kcal</span>
+        <div className={"card meal"}>
+            <div className={"card-header mealCard_divider"}>
+                <div className={"card-header-wrapper"}>
+                    <label>{props.meal.name}</label>
+                    <span className={"gradient"}><span
+                        className={"serving"}>{props.meal.totalCalories}</span> kcal</span>
+                </div>
+            </div>
+            <div className={"meal_mealItem-wrapper"}>
+                {renderMealItems()}
+                {props.meal.mealItems.length > 2 && <span className={"meal_mealItem"}>...</span>}
+            </div>
         </div>
     );
 }
