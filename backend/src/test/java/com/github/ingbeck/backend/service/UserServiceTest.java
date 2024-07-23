ac0d91987b9cc7fef88161a0c9bedab59533514e
ingobecker <mail@ingobecker.de>
2024-07-23 12:37:28 +0200

    add unit test

--- backend/src/test/java/com/github/ingbeck/backend/service/UserServiceTest.java
+++ backend/src/test/java/com/github/ingbeck/backend/service/UserServiceTest.java
@@ -53,6 +53,18 @@ class UserServiceTest {
         assertThrows(NoSuchElementException.class, () -> service.createUser("2", appUserCreateDto));
     }
 
+    @Test
+    void createDemoUser_whenCalled_returnValidUser(){
+        //GIVEN
+        //THEN
+        service.createDemoUser("Max");
+
+        //WHEN
+        verify(repo).save(any(AppUser.class));
+        verify(diaryService).createDemoDiary(any());
+    }
+
+
     private static AppUser getAppUser(AppUserCreateDto appUserCreateDto) {
         int bmr = (int)(66.47 + 13.75 * appUserCreateDto.weight() + 5.003 * appUserCreateDto.height() - 6.755 * 32);
         int bmrWithActivity = (int)(bmr * appUserCreateDto.activityLevel().getLevel());
@@ -72,5 +84,6 @@ class UserServiceTest {
                 false
         );
     }
+
 
 }
